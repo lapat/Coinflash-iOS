9c0648e052bc7cafe37c51b6cb59516c9e14fa83
tallal <tallal.javed@outlook.com>
2017-10-05 19:47:33 +0500

    update new views

--- coinflash/AccountSettingVC.swift
+++ coinflash/AccountSettingVC.swift
@@ -9,6 +9,27 @@
 import Foundation
 import UIKit
 
-class AccountSettingsVC: UIViewController{
+class AccountSettingsVC: UIViewController, UITableViewDataSource{
+    @IBOutlet weak var bankTable: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell!
+        if indexPath.row == 0{
+            cell = tableView.dequeueReusableCell(withIdentifier: "normalCell")
+        }
+        else{
+            cell = tableView.dequeueReusableCell(withIdentifier: "disabledCell")
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
 }
