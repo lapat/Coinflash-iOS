75340e2243cf21bb9c36d0a092ebbd97b988a562
Rana Tallal <eagle_phoenix2@hotmail.com>
2017-10-08 05:57:48 +0500

    Login api done with progress bar.

---
+++ coinflash/HelperFunctions.swift
@@ -0,0 +1,16 @@
+//
+//  HelperFunctions.swift
+//  coinflash
+//
+//  Created by Tallal Javed on 10/8/17.
+//  Copyright © 2017 CoinFlash. All rights reserved.
+//
+
+import UIKit
+
+class HelperFunctions: NSObject {
+
+    static func addLoadingScreen(view: UIViewController){
+        
+    }
+}
